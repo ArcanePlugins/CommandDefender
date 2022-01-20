2fec219629a9317ac06f2ba59ffcac7a1b9c6ed8
lokka30 <lokka30@protonmail.com>
2022-01-20 17:35:11 +0800

    update

--- core-bukkit/src/main/java/me/lokka30/commanddefender/corebukkit/listener/PlayerCommandPreprocessListener.java
+++ core-bukkit/src/main/java/me/lokka30/commanddefender/corebukkit/listener/PlayerCommandPreprocessListener.java
@@ -1,11 +1,19 @@
 package me.lokka30.commanddefender.corebukkit.listener;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class PlayerCommandPreprocessListener implements Listener, ListenerInfo {
 
     @Override
     public boolean compatibleWithServer() {
         return true;
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onPlayerCommandPreprocess(final PlayerCommandPreprocessEvent event) {
+        //TODO
     }
 }
--- core-bukkit/src/main/java/me/lokka30/commanddefender/corebukkit/listener/PlayerCommandSendListener.java
+++ core-bukkit/src/main/java/me/lokka30/commanddefender/corebukkit/listener/PlayerCommandSendListener.java
@@ -1,12 +1,20 @@
 package me.lokka30.commanddefender.corebukkit.listener;
 
 import me.lokka30.commanddefender.corebukkit.util.BukkitUtils;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandSendEvent;
 
 public class PlayerCommandSendListener implements Listener, ListenerInfo {
 
     @Override
     public boolean compatibleWithServer() {
         return BukkitUtils.serverHasPlayerCommandSendEvent();
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onPlayerCommandSend(final PlayerCommandSendEvent event) {
+        //TODO
     }
 }
