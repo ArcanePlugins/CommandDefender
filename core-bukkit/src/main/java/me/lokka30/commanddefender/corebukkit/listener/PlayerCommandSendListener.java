10e55509d4bff915782ff0c90538143bd243bd8c
lokka30 <lokka30@protonmail.com>
2022-02-08 19:09:14 +0800

    update

--- core-bukkit/src/main/java/me/lokka30/commanddefender/corebukkit/listener/PlayerCommandSendListener.java
+++ core-bukkit/src/main/java/me/lokka30/commanddefender/corebukkit/listener/PlayerCommandSendListener.java
@@ -1,5 +1,6 @@
 package me.lokka30.commanddefender.corebukkit.listener;
 
+import java.util.LinkedList;
 import me.lokka30.commanddefender.core.Commons;
 import me.lokka30.commanddefender.core.debug.DebugCategory;
 import me.lokka30.commanddefender.core.debug.DebugHandler;
@@ -19,19 +20,39 @@ public class PlayerCommandSendListener implements ListenerExt {
 
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onPlayerCommandSend(final PlayerCommandSendEvent event) {
-        if (DebugHandler.isDebugCategoryEnabled(DebugCategory.COMMAND_SUGGESTION_LISTENER)) {
+
+        final boolean debugLog = DebugHandler.isDebugCategoryEnabled(DebugCategory.COMMAND_SUGGESTION_LISTENER);
+        LinkedList<String> originalForDebugging = null;
+
+        if (debugLog) {
             Commons.core().logger().debug(DebugCategory.COMMAND_SUGGESTION_LISTENER, String.format(
-                "%s is being sent the commands %s",
+                "&f%s&7 is being sent the commands &8(&cbefore&7 interception&8)&7: &8[&b%s&8] (&b%s &7items&8)&7.",
                 event.getPlayer().getName(),
-                event.getCommands()
+                String.join("&7, &b", event.getCommands()),
+                event.getCommands().size()
             ));
+
+            originalForDebugging = new LinkedList<>(event.getCommands());
         }
 
-        event.getCommands().removeIf(command -> Commons.core().commandFilter().canAccess(
+        event.getCommands().removeIf(command -> !Commons.core().commandFilter().canAccess(
             FilterContextType.COMMAND_SUGGESTION,
             BukkitPlatformHandler.bukkitPlayerToUniversal(event.getPlayer()),
             ("/" + command).split(" ")
         ));
+
+        if (debugLog) {
+            originalForDebugging.removeAll(event.getCommands());
+
+            Commons.core().logger().debug(DebugCategory.COMMAND_SUGGESTION_LISTENER, String.format(
+                "&f%s&7 is being sent the commands &8(&aafter&7 interception&8)&7: &8[&b%s&8] (&b%s &7items&8)&7. The following items were removed: &8[&b%s&8] (&b%s &7items&8)&7.",
+                event.getPlayer().getName(),
+                String.join("&7, &b", event.getCommands()),
+                event.getCommands().size(),
+                originalForDebugging,
+                originalForDebugging.size()
+            ));
+        }
     }
 
 }
